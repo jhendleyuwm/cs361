5e2d52daebad38f2ea7671f8cde5b49662d9396f
beckerej <beckerej@ceas-942-mac02.cae.uwm.edu>
2016-02-02 14:46:05 -0600

    Update

---
+++ src/Frame.java
@@ -0,0 +1,43 @@
+
+public class Frame 
+{
+	private boolean _isStrike;
+	private boolean _isSpare;
+	private int _value;
+	private int _throw1;
+	private int _throw2;
+	
+	public Frame()
+	{
+		
+	}
+	
+	public void setStrike(boolean b){
+		
+	}
+	
+	public boolean getStrike(){
+		return this._isStrike;
+	}
+	
+	public void setSpare(boolean b){
+		
+	}
+	
+	public boolean getSpare(){
+		return this._isSpare;
+	}
+	
+
+	
+	public int getValue(){
+		return this._value;
+	}
+	
+	public void setValue(int i)
+	{
+		this._value = i;
+	}
+	
+	
+}
